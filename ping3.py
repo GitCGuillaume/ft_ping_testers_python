ee60c1d6266542ba835ac93cd976f29d7193558e
gchopin <guillaume2.chopin@orange.fr>
2024-02-06 10:04:25 +0100

    correctif

--- ping3.py
+++ ping3.py
@@ -8,33 +8,34 @@ def spoof_reply(packet):
     global i
     global s
     pkt = IP(packet.get_payload())
-    dst=pkt[0].dst
-    pkt.show2()
-    src=pkt[0].src
-    seq = pkt[1].seq
-    #seq = 22
-    id = pkt[1].id
-    #id = 1111
-    load=pkt[2].load
-    ip = IP(src=dst, dst=src)
-    icmp = ICMP(type=0, id=id, seq=seq)
-    icmp2 = ICMP(type=0, id=id, seq=i)
-    i = i + 1
-    reply = ip/icmp/load
-    #reply2 = ip/icmp2/load
-    #nbPacketRcv > nbPacketSent = display packet forged
-    reply2 = ip/icmp2/load
-    send(reply2)
-    if s == 1 and i == 102:
-        send(ip/ICMP(type=0, id=id, seq=1)/load)
-    elif s == 0 and i == 102:
-        print(i)
-        s = 1
-    elif i == 105:
-        i = 100
-    else:
-        print("sss")
-        send(reply)
+    if (pkt[1].type == 8):
+        dst=pkt[0].dst
+        pkt.show2()
+        src=pkt[0].src
+        seq = pkt[1].seq
+        #seq = 22
+        id = pkt[1].id
+        #id = 1111
+        load=pkt[2].load
+        ip = IP(src=dst, dst=src)
+        icmp = ICMP(type=0, id=id, seq=seq)
+        icmp2 = ICMP(type=0, id=id, seq=i)
+        i = i + 1
+        reply = ip/icmp/load
+        #reply2 = ip/icmp2/load
+        #nbPacketRcv > nbPacketSent = display packet forged
+        reply2 = ip/icmp2/load
+        send(reply2)
+        if s == 1 and i == 102:
+            send(ip/ICMP(type=0, id=id, seq=1)/load)
+        elif s == 0 and i == 102:
+            print(i)
+            s = 1
+        elif i == 105:
+            i = 100
+        else:
+            print("sss")
+            send(reply)
      #   time.sleep(3)
      #   send(reply)
 
--- wr_load_chk.py
+++ wr_load_chk.py
@@ -4,19 +4,20 @@ import time
 
 def spoof_reply(packet):
     pkt = IP(packet.get_payload())
-    dst=pkt[0].dst
-    #pkt.show2()
-    src=pkt[0].src
-    seq = pkt[1].seq
-    #seq = 22
-    id = pkt[1].id
-    #id = 1111
-    #load=pkt[2].load
-    ip = IP(src=dst, dst=src)
-    #icmp = ICMP(type=0, id=id, seq=seq)
-    #reply = ip/icmp/load
-    #reply[ICMP].chksum = 0x82e2
-    send(ip/ICMP(type=0, id=id, seq=seq)/"123456789123132545646545645646545465456456456445465456465465465456456456456")
+    if (pkt[1].type == 8):
+        dst=pkt[0].dst
+        #pkt.show2()
+        src=pkt[0].src
+        seq = pkt[1].seq
+        #seq = 22
+        id = pkt[1].id
+        #id = 1111
+        #load=pkt[2].load
+        ip = IP(src=dst, dst=src)
+        #icmp = ICMP(type=0, id=id, seq=seq)
+        #reply = ip/icmp/load
+        #reply[ICMP].chksum = 0x82e2
+        send(ip/ICMP(type=0, id=id, seq=seq)/"123456789123132545646545645646545465456456456445465456465465465456456456456")
     
 if __name__=="__main__":
     nfqueue = NetfilterQueue()
